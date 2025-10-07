9d1301a0cc71254f3f73cabf68581b26825d1e8e
Szymon Kadaś <kadasszymon@gmail.com>
2025-10-07 16:17:47 +0200

    feat: songs application layer event handlers

---
+++ src/Application/Songs/EventHandlers/SongPlayedEventHandler.cs
@@ -0,0 +1,21 @@
+using CleanArchitecture.Domain.Events.SongEvents;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.Application.Songs.EventHandlers;
+
+public class SongPlayedEventHandler : INotificationHandler<SongPlayedEvent>
+{
+    private readonly ILogger<SongPlayedEventHandler> _logger;
+
+    public SongPlayedEventHandler(ILogger<SongPlayedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(SongPlayedEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Song played: {SongId}", notification.SongId);
+        return Task.CompletedTask;
+    }
+}
